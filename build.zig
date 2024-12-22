0ee0eb7f3c3876aa7ee75a5b16ffba6c9b42a59c
Vincent Rischmann <vincent@rischmann.fr>
2024-12-22 01:38:26 +0100

    initial commit

---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // NOTE(vincent): the upstream module containing the actual C library is also named picohttpparser.
+
+    const picohttpparser = b.dependency("picohttpparser", .{});
+
+    const mod = b.addModule("picohttpparser", .{
+        .root_source_file = b.path("picohttpparser.zig"),
+        .target = target,
+        .optimize = optimize,
+        // .link_libc = true,
+    });
+    mod.addIncludePath(picohttpparser.path("."));
+    mod.linkLibrary(picohttpparser.artifact("picohttpparser"));
+
+    const tests = b.addTest(.{
+        .root_source_file = b.path("picohttpparser.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    tests.addIncludePath(picohttpparser.path("."));
+    tests.linkLibrary(picohttpparser.artifact("picohttpparser"));
+
+    const run_tests = b.addRunArtifact(tests);
+    const test_step = b.step("test", "Run the tests");
+    test_step.dependOn(&run_tests.step);
+}
